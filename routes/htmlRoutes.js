ed7176c9fb79109d28ebc425729661c5bb305cae
Misty Duhart <mystie.smith@gmail.com>
2022-05-16 21:45:41 -0400

    changed order of route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,14 +2,15 @@
 const path = require('path');
 const router = require('express').Router();
 
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
 // This has to be on the top than the other routers
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-});
 
 // If no matching route is found default to home page
 router.get('*', (req, res) => {
