181de61215426f6118ef31da3f067b0ccba8658a
Misty Duhart <mystie.smith@gmail.com>
2022-05-16 22:18:14 -0400

    made changes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,18 +3,18 @@ const path = require('path');
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, '../develop/public/index.html'));
 });
 
 // This has to be on the top than the other routers
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
+    res.sendFile(path.join(__dirname, '../develop/public/notes.html'));
 });
 
 
 // If no matching route is found default to home page
 router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, '../develop/public/index.html'));
 });
 
 
